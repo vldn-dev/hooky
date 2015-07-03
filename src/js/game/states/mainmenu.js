5a58e2b7ac3f95477c9697319a50960dc9b8295e
Sven v.d.Velden <sven.vdv90@gmail.com>
2015-07-03 19:10:53 +0200

    +

--- src/js/game/states/mainmenu.js
+++ src/js/game/states/mainmenu.js
@@ -1,63 +1,74 @@
 var mainmenu = {};
-mainmenu.preload = function(){
+mainmenu.preload = function() {
+    this.game.load.spritesheet('button', 'images/button_sprite_sheet.png', 193, 71);
+};
+var roomid;
+var roomname;
+var button;
+
+mainmenu.create = function() {
+    var hash = window.location.hash.replace('#', '');
+    if (!hash.length) {
+        var namegroup = this.game.add.group();
+        var idgroup = this.game.add.group();
+
+
+        button = this.game.add.button(632, 32, 'button', start, this, 2, 1, 0);
+
+        var style = {
+            font: "24px Arial",
+            fill: "#ffffff",
+            align: "center"
+        };
+
+        var y = 20;
+        var xmlhttp = new XMLHttpRequest();
+        var url = "http://vldn.de/get.php";
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                myFunction(xmlhttp.responseText);
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+
+        function myFunction(response) {
+            var arr = JSON.parse(response);
+            var i;
+
+            for (i = 0; i < arr.length; i++) {
+                roomid = mainmenu.game.add.text(32, y, arr[i].ID, style, idgroup);
+                roomname = mainmenu.game.add.text(62, y, arr[i].User, style, namegroup);
+                roomname.name = i;
+                namegroup.children[i].inputEnabled = true;
+                namegroup.children[i].events.onInputDown.add(open, this);
+                y += 24;
+            }
+
+
+
+        }
+
+    } else {
+
+        this.game.state.start('game');
+    }
+
 
 };
-var info;
 
-mainmenu.create = function (){
+function open(target) {
+    var t = target;
+    location.href = location.href + '#' + t.text;
+    location.reload();
 
-  var hash = window.location.hash.replace('#','');
-  if (!hash.length){
+    console.log(t.text + 'clicked');
+}
 
-	var style = { font: "24px Arial", fill: "#ffffff", align: "center" };
+function start() {
 
-	var y = 20;
-	var xmlhttp = new XMLHttpRequest();
-	var url = "http://vldn.de/get.php";
-
-	xmlhttp.onreadystatechange=function() {
-	  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		myFunction(xmlhttp.responseText);
-	  }
-	};
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
-
-	function myFunction(response) {
-	  var arr = JSON.parse(response);
-	  var i;
-	  var out = "<table>";
-
-	  for(i = 0; i < arr.length; i++) {
-
-		info = mainmenu.game.add.text(32, y, arr[i].ID, style);
-		info = mainmenu.game.add.text(62, y, arr[i].User, style);
-		//	  out += "<tr><td>" +
-		//		arr[i].ID +
-		//		"</td><td>" +
-		//		arr[i].User +
-		//		"</td><td>" ;
-		y += 24;
-	  }
-	  //	out += "</table>";
-	  //	document.getElementById("id01").innerHTML = out;
-	}
-
-
-	//location.href = location.href + '#' + 'blablablaa';
-	//location.reload();
-  }
-  else{
-
-	this.game.state.start('game');}
-
-
-
-
-
-
-
-
-	//this.game.state.start('game');
-};
-module.exports=mainmenu;
+    location.href = location.href + '#' + new Date().getTime();
+    location.reload();
+}
+module.exports = mainmenu;
